ce575f1f133baaf40e494a84bce766c493153156
Zeynep Nur Bağcı <zynpnr44@gmail.com>
2026-07-28 23:04:20 +0300

    Create AtaSinif class with constructor and method

---
+++ NesneYonelimliProgramlama/AtaSinif.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Windows.Forms;
+
+namespace NesneTabanliProgramlama.Hafta3
+{
+    public class AtaSinif
+    {
+        public AtaSinif()
+        {
+            MessageBox.Show("Ben ata sınıfının kurucusuyum");
+        }
+
+        public void Yaz()
+        {
+            MessageBox.Show("Ben ata sınıfından yazıyorum");
+        }
+    }
+}
